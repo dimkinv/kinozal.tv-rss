d3f813e63e27b685f482ce3a10b13ff0fde46b5b
Danny Vernovsky <dvernovsky@interwise.com>
2015-03-05 17:26:15 +0200

    step 1

--- routes/rss.js
+++ routes/rss.js
@@ -3,22 +3,51 @@ var request = require('superagent');
 var xml = require('xml2js');
 var q = require('q');
 var _ = require('lodash');
+var NodeCache = require('node-cache');
 
 var router = express.Router();
+var cache = new NodeCache();
 var idRegex = /(?!\?id=)\d+/g;
+var torrentBaseUrl = 'http://kinozal.tv/download.php/{0}/[kinozal.tv]id{0}.torrent';
+
 
 /* GET users listing. */
-router.get('/:cridentials', function (req, res) {
+router.post('/test', function (req, res) {
+    console.log(req.headers);
+    console.log(req.body);
+    res.send('');
+});
+
+router.get('/link/:cridentials/:movieId/file.torrent', function (req, res) {
     var parts = req.params.cridentials.split(':');
     var cridentials = {
         username: parts[0],
         password: parts[1]
     };
 
+    var authenticationDetails = cache.get(req.params.cridentials);
+    console.log(authenticationDetails);
+    if (!_.isEmpty(authenticationDetails)) {
+        retrieveTorrentFile(req.params.movieId).then(function (torrentFile) {
+            //send file
+            res.send(torrentFile);
+        });
+        return;
+    }
+
+    login(cridentials, req.params.movieId)
+        .then(retrieveTorrentFile)
+        .then(function (torrentFile) {
+            //send file
+            res.send(torrentFile);
+        });
+});
+
+router.get('/:cridentials/rss.xml', function (req, res) {
     getRSS()
         .end(function (err, response) {
             parseXML(err, response)
-                .then(buildLinks)
+                .then(buildLinks.bind(null, req.params.cridentials))
                 .then(function (links) {
                     res.json(links);
                 })
@@ -53,7 +82,7 @@ function parseXML(err, res) {
     return deferred.promise;
 }
 
-function buildLinks(obj) {
+function buildLinks(cridentials, obj) {
     var links = _.map(obj.rss.channel[0].item, function (item) {
         var matches = item.link[0].match(idRegex);
         if (matches.length === 0) {
@@ -61,11 +90,47 @@ function buildLinks(obj) {
         }
         return {
             title: item.title,
-            pageLink: 'http://localhost:3000/rss/link/' + matches[0]
+            pageLink: 'http://localhost:3000/rss/link/' + cridentials + '/' + matches[0] + '/file.torrent'
         }
     });
 
     return links;
+}
+
+function retrieveTorrentFile(movieId) {
+    var deferred = q.defer();
+
+    var torrentUrl = torrentBaseUrl.replace(/\{0\}/g, movieId);
+    //request.get(torrentUrl)
+    //    .set
+    deferred.resolve(torrentUrl);
+
+    return deferred.promise;
+}
+
+function login(cridentials, movieId) {
+    var deferred = q.defer();
+
+    request.post('http://kinozal.tv/takelogin.php')
+        .send(cridentials)
+        .type('form')
+        .redirects(0)
+        .end(function (err, res) {
+            if (err) {
+                deferred.reject(err);
+                return;
+            }
+            console.log(res.headers);
+            var authentication = {
+                uid: res.headers['set-cookie'][0].match(/uid=\d+;/g)[0],
+                pass: res.headers['set-cookie'][1].match(/pass=\w+;/g)[0]
+            };
+            console.log(authentication);
+            cache.set(cridentials.username + ':' + cridentials.password, authentication);
+            deferred.resolve(movieId);
+        });
+
+    return deferred.promise;
 }
 
 module.exports = router;
